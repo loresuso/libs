9b689bc07663e0112fe60753526615f7f1ae4cb0
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-05-30 13:44:23 +0000

    update(libscap/engine/gvisor): move runsc handling in its own class
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

---
+++ userspace/libscap/engine/gvisor/runsc.cpp
@@ -0,0 +1,136 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "runsc.h"
+
+runsc_manager::runsc_manager(std::string root_path, std::string trace_session_config_path):
+		m_root_path(root_path), m_trace_session_config_path(trace_session_config_path)
+{
+
+}
+
+bool runsc_manager::start_trace_session()
+{
+	
+}
+
+std::vector<std::string> runsc_manager::runsc(char *argv[])
+{
+	std::vector<std::string> res;
+	int pipefds[2];
+	int line_size = 512;
+
+	int ret = pipe(pipefds);
+	if(ret)
+	{
+		return res;
+	}
+
+	int pid = fork();
+	if(pid > 0)
+	{
+		char line[line_size];
+		int status;
+		
+		::close(pipefds[1]);
+		wait(&status);
+		if(status)
+		{
+			return res;
+		}
+
+		FILE *f = fdopen(pipefds[0], "r");
+		if(!f)
+		{
+			return res;
+		}
+
+		while(fgets(line, line_size, f))
+		{
+			res.emplace_back(std::string(line));
+		}
+
+		fclose(f);
+	}
+	else
+	{
+		::close(pipefds[0]);
+		dup2(pipefds[1], STDOUT_FILENO);
+		execvp("runsc", argv);
+		exit(1);
+	}
+
+	return res;
+}
+
+void runsc_manager::runsc_list()
+{
+	const char *argv[] = {
+		"runsc", 
+		"--root",
+		m_root_path.c_str(),
+		"list",
+		NULL
+	};
+
+	std::vector<std::string> output = runsc((char **)argv);
+
+	for(auto &line : output)
+	{
+		if(line.find("running") != std::string::npos)
+		{
+			std::string sandbox = line.substr(0, line.find_first_of(" ", 0));
+			m_running_sandboxes.emplace_back(sandbox);
+		}
+	}
+}
+
+void runsc_manager::runsc_trace_create(std::string sandbox_id)
+{
+	const char *argv[] = {
+		"runsc", 
+		"--root",
+		m_root_path.c_str(),
+		"trace",
+		"create",
+		"--force",
+		"--config", 
+		m_trace_session_config_path.c_str(),
+		sandbox_id.c_str(),
+		NULL
+	};
+
+	std::vector<std::string> output = runsc((char **)argv);
+}
+
+void runsc_manager::runsc_trace_procfs(std::string sandbox_id)
+{
+	const char *argv[] = {
+		"runsc", 
+		"--root",
+		m_root_path.c_str(),
+		"trace",
+		"procfs",
+		sandbox_id.c_str(),
+		NULL
+	};
+
+	std::vector<std::string> output = runsc((char **)argv);
+}
---
+++ userspace/libscap/engine/gvisor/runsc.h
@@ -0,0 +1,38 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+class runsc_manager {
+public:
+	runsc_manager(std::string root_path, std::string trace_session_config_path);
+
+	bool start_trace_session();
+
+private:
+	std::vector<std::string> runsc(char *argv[]);
+	void runsc_list();
+    void runsc_trace_create(std::string sandbox_id);
+    void runsc_trace_procfs(std::string sandbox_id);
+
+	std::string m_root_path;
+	std::string m_trace_session_config_path;
+	std::vector<std::string> m_running_sandboxes;
+};
